ad8c1726f18dcff3ac7cf7d8695c059a4cfde70c
Guillaume Thomas <guillaume.thomas2@gmail.com>
2023-04-09 14:07:12 +0200

    feat(production): Add security with oAuth2 protocole

--- production/api/src/index.py
+++ production/api/src/index.py
@@ -1,11 +1,24 @@
 import io
 import logging
+import os
 
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, Depends
+from fastapi.security import OAuth2PasswordBearer
 from mlopspython_inference.model_pillow import Model
+from oidc_jwt_validation.authentication import Authentication
+from oidc_jwt_validation.http_service import ServiceGet
 
 app = FastAPI()
 model = Model(logging, "./production/api/resources/final_model.h5")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+issuer = os.getenv("OAUTH2_ISSUER")
+audience = os.getenv("OAUTH2_AUDIENCE")
+jwks_uri = os.getenv("OAUTH2_JWKS_URI")
+logger = logging.getLogger(__name__)
+authentication = Authentication(logger, issuer, ServiceGet(logger), jwks_uri)
+skip_oidc = False
 
 
 @app.get("/health")
@@ -14,7 +27,9 @@ def health():
 
 
 @app.post("/upload")
-async def upload(file: UploadFile):
+async def upload(file: UploadFile, token: str = Depends(oauth2_scheme)):
+    if not skip_oidc:
+        await authentication.validate_async(token, audience, "get::prediction")
     file_readed = await file.read()
     file_bytes = io.BytesIO(file_readed)
     return model.execute(file_bytes)
